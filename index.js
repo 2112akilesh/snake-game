4e81de52cc971746acb204ccbf09af0719772c8e
2112akilesh <2112akilesh@gmail.com>
2020-05-10 00:52:18 +0300

    2 files modified

--- index.js
+++ index.js
@@ -2,7 +2,25 @@
 import './style.css';
 
 // Write Javascript code!
-const canvas = document.getElementsByTagName('camvas')[0].getContext('2d');
-canvas.fillStyle('white');
-canvas.fillR
+const canvas = document.getElementsByTagName('canvas')[0].getContext('2d');
+const snake = new Snake();
 
+
+
+function Snake(){
+  this.x = 0;
+  this.y = 0;
+  this.dx = 1;
+  this.dy = 0;
+  this.width = 10;
+  this.height = 10;
+}
+
+setInterval(()=>{
+canvas.fillStyle='white';
+canvas.fillRect(this.x,this.y,this.width,this.height);
+})
+
+function update(){
+  
+}
