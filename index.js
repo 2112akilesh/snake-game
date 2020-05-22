022d9feca3f2949c587cd77ac096865a2ad7ac02
2112akilesh <2112akilesh@gmail.com>
2020-05-22 14:04:43 +0530

    1 file modified

--- index.js
+++ index.js
@@ -9,25 +9,22 @@ const canvasHeight = 200;
 const canvasWidth = 200;
 let food;
 
-document.addEventListener ('keydown', (event) => {
+document.addEventListener('keydown', (event) => {
   if(event.keycode == 37){
     //left
     snake.dx = -1;
     snake.dy = 0;
-  }
-  else if(event.keycode == 38){
+  } else if(event.keycode == 38){
     //up
-    snake.dy = 1;
+    snake.dy = -1;
     snake.dx = 0;
-  }
-  else if(event.keycode == 39){
+  } else if(event.keycode == 39){
     //right  
     snake.dx = 1; 
     snake.dy = 0; 
-  }  
-  else if(event.keycode == 40){
+  }  else if(event.keycode == 40){
     //down    
-    snake.dy = -1;
+    snake.dy = 1;
     snake.dx = 0;
   }
 }) 
